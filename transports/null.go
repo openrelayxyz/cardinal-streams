75260b138da9368038bf8f074a82c45d9499b5c4
Austin Roberts <austin.roberts@openrelay.xyz>
2021-09-29 12:30:38 -0500

    Add null transport for starting up without a messaging subsystem

---
+++ transports/null.go
@@ -0,0 +1,35 @@
+package transports
+
+import "github.com/openrelayxyz/cardinal-types"
+
+type nullConsumer struct{}
+
+func NewNullConsumer() Consumer { return &nullConsumer{} }
+
+func (*nullConsumer) Start() error {
+  return nil
+}
+func (*nullConsumer) Subscribe(ch interface{}) types.Subscription {
+  return nullSubscription{}
+}
+func (*nullConsumer) SubscribeReorg(ch chan<- map[int64]types.Hash) types.Subscription {
+  return nullSubscription{}
+}
+func (*nullConsumer) Close() {}
+
+func (*nullConsumer) Ready() <-chan struct{} {
+  ch := make(chan struct{}, 1)
+  ch <- struct{}{}
+  return ch
+}
+func (*nullConsumer) WhyNotReady(hash types.Hash) string {
+  return "null"
+}
+
+
+type nullSubscription struct{}
+
+func (nullSubscription) Unsubscribe() {}
+func (nullSubscription) Err() <-chan error {
+  return make(chan error)
+}
